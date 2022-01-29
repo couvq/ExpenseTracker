c8f69128d931e54317b1df72f81bf122c8eacc82
couvelaireq <quentin.couvelaire@hope.edu>
2022-01-29 10:37:32 -0500

    added handlers for all states using useState, a bit messy

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     const titleChangeHandler = (event) => {
         // get the title the user entered
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
     };
+
+    const amountChangeHandler = (event) => {
+        // get the amount the user entered
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        // get the date the user entered
+        setEnteredDate(event.target.value);
+    }
 
     return (
     <form>
@@ -16,11 +30,11 @@ const ExpenseForm = () => {
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="date"  min="2019-01-01" max="2022-12-31" />
+                <input type="date"  min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
