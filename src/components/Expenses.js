9c3b3c7817f06621937f89afd946bfae031d4f0a
couvelaireq <quentin.couvelaire@hope.edu>
2022-02-04 09:28:24 -0500

    added key for each ExpenseItem mapped

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -21,7 +21,14 @@ function Expenses(props) {
           selected={yearState}
           onSelectYear={onSelectYearHandler}
         />
-        {props.expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
